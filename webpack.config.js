6b147e3bc12c1caa90fe77f5f947e700722ce067
varxyz <mrsergiumoraru@gmail.com>
2018-09-08 18:53:56 +0300

    edit 404

--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,7 @@ module.exports = {
     // new OfflinePlugin(),
   ],
   devServer: {
-    historyApiFallback: true,
+    // historyApiFallback: true,
     host: '0.0.0.0',
     port: 8080,
     // disableHostCheck: true,
